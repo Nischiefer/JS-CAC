ac1aff2da175edab80237320ea15cb945063dd42
Nischal Shrestha <nischal143no1@gmail.com>
2024-12-14 23:25:07 +0545

    committed

--- 01-basics/02_dataTypes.js
+++ 01-basics/02_dataTypes.js
@@ -1,1 +1,21 @@
 // fnm env --use-on-cd | Out-String | Invoke-Expression
+"use strict";        //treat all JS code as newer version
+// alert("Hello");      //we are using nodejs, not browser
+
+
+let name="Nischal"
+let age=21
+let isLoggedin=false
+
+//number =>2 to the power 53
+//bigint
+//string ==>""
+//boolean => true/false
+//null ==> stand alone value        type of null=object
+//undefined=>   type of undefined=undefined
+//symbol=> unique nedd find garna lai specially in react/figma
+
+//object ==>
+
+console.log(typeof undefined)
+console.log(typeof null)
