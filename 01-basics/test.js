de7eff711e0d0348fb819091e332e5f85515470f
Nischal Shrestha <nischal143no1@gmail.com>
2024-12-14 23:03:00 +0545

    pls comment

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,2 +1,10 @@
-console.log("Nischal");
 // fnm env --use-on-cd | Out-String | Invoke-Expression
+console.log("Nischal");
+const accId = 123;      //constant
+let accEml= "nis@gmail.com"     //variable which can be reasigned later but cant be redeclared
+var accPas="123"                //it cant be reassigned but can be re declared  //var birsidiye hucnha use nagarekai best
+accCt="ktm"
+
+// accId=2
+console.log(accId)
+console.table([accId,accEml,accPas,accCt])
